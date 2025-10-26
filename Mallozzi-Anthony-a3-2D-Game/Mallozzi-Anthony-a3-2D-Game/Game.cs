9f0beaddb7583facb42f8325adda6362a61d56fe
Anthony-Mlzi <000982050@mohawkcollege.ca>
2025-10-26 16:04:23 -0400

    Added player left and right movement.

--- Mallozzi-Anthony-a3-2D-Game/Mallozzi-Anthony-a3-2D-Game/Game.cs
+++ Mallozzi-Anthony-a3-2D-Game/Mallozzi-Anthony-a3-2D-Game/Game.cs
@@ -1,4 +1,5 @@
 ﻿// Include the namespaces (code libraries) you need below.
+using Raylib_cs;
 using System;
 using System.Numerics;
 
@@ -12,7 +13,11 @@ namespace MohawkGame2D
     {
         // Place your variables here:
 
+        Vector2 playerPosition = new(400, 130);
+        Vector2 playerSize = new(15, 25);
 
+        Vector2 gravity = new(0, 500);
+        Vector2 velocity = new(0, 0);
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
@@ -28,6 +33,65 @@ namespace MohawkGame2D
         public void Update()
         {
             Window.ClearBackground(Color.DarkGray);
+
+            Grate();
+            Player();
+
+        }
+        public void Gravity()
+        {
+
+        }
+        public void Player()
+        {
+            float moveSpeed = 10;
+
+            Draw.LineSize = 2;
+            Draw.FillColor = Color.White;
+            Draw.Rectangle(playerPosition, playerSize);
+
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Left))
+            {
+                playerPosition.X -= moveSpeed;
+                Console.WriteLine("left");
+                if (playerPosition.X < 0)
+                {
+                    playerPosition.X = 0;
+                }
+            }
+            else if (Input.IsKeyboardKeyDown(KeyboardInput.Right))
+            {
+                playerPosition.X += moveSpeed;
+                Console.WriteLine("right");
+                if (playerPosition.X > 785)
+                {
+                    playerPosition.X = 785;
+                }
+            }
+            else if (Input.IsKeyboardKeyDown(KeyboardInput.Space))
+            {
+                Console.WriteLine("jump");
+
+                velocity += gravity * Time.DeltaTime;
+                playerPosition -= velocity * Time.DeltaTime;
+            }
+
+        }
+        public void Grate()
+        {
+
+            for (int i = 0; i < 41; i++)
+            {
+                Vector2 gratePosition = new Vector2(0, 100);
+                Vector2 grateSize = new Vector2(10, 75);
+
+                gratePosition.X += i * 20 + 5;
+
+                Draw.LineColor = Color.Black;
+                Draw.LineSize = 2;
+                Draw.FillColor = Color.Black;
+                Draw.Rectangle(gratePosition, grateSize);
+            }
         }
     }
 
